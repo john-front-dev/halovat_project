704ce2d6e6d2505127dd84bb9f4445967e7b665d
JOHN24108909842 <jamshid200404@gmail.com>
2023-10-30 12:49:17 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+<style>
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex justify-start items-center h-[91px] border-b">
+    <div class="w-[117px] min-w-[117px] min-h-[53px] h-[53px] ml-[72px]">
+        <img src="../assets/logo.png" alt="">
+    </div>
+    <ul class="flex items-center ml-[272px] list-none text-[#272727] text-[16px] font-normal gap-[45px]">
+        <li><router-link to="/">Главная</router-link></li>
+        <li>О компании</li>
+        <li>Продукция</li>
+        <li>Партнёрам</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+li{
+    @apply cursor-pointer
+}
+</style>
